eb0a7629723213d2f332eeca5bf9a01c60f220f5
Ilya <iovodovi@yandex.ru>
2025-02-28 17:33:09 +0300

    Создал конфиг для подключения к бд по параметрам из конфига

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config';
+
+export const getMongoConfig = async (configService: ConfigService) => ({
+	uri: getMongoString(configService),
+});
+
+const getMongoString = (configService: ConfigService) => {
+	const login: string | undefined = configService.get('MONGO_LOGIN');
+	const password: string | undefined = configService.get('MONGO_PASSWORD');
+	const host: string | undefined = configService.get('MONGO_HOST');
+	const port: string | undefined = configService.get('MONGO_PORT');
+	const defaultHub: string | undefined = configService.get('MONGO_AUTHDB');
+
+	return `mongodb://${login}:${password}@${host}:${port}/${defaultHub}`;
+};
