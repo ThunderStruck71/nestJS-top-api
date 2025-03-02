a34fc2270eae63595031e3c7f9c542d8826e5a57
Ilya <iovodovi@yandex.ru>
2025-03-02 20:25:32 +0300

    Добавил сервис для обработки запросов по отзывам

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { ReviewModel } from './review.model';
-import { Model } from 'mongoose';
+import { ReviewDocument, ReviewModel } from './review.model';
+import { Model, Types } from 'mongoose';
+import { CreateReviewDto } from './dto/create-review.dto';
 
 @Injectable()
 export class ReviewService {
-	constructor(@InjectModel(ReviewModel.name) private readonly reviewModel: Model<ReviewModel>) {}
+	constructor(@InjectModel(ReviewModel.name) private readonly reviewModel: Model<ReviewDocument>) {}
+
+	async create(dto: CreateReviewDto) {
+		const createdReview = new this.reviewModel({
+			...dto,
+			productId: new Types.ObjectId(dto.productId),
+		});
+		return createdReview.save();
+	}
+
+	async delete(id: string) {
+		return await this.reviewModel.findByIdAndDelete(id).exec();
+	}
+
+	async findByProductId(productId: string) {
+		return this.reviewModel.find({ productId: new Types.ObjectId(productId) }).exec();
+	}
+
+	async deleteByProductId(productId: string) {
+		return this.reviewModel.deleteMany({ productId: new Types.ObjectId(productId) }).exec();
+	}
 }
