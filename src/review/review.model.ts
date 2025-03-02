d73b456aad0fcbc06ba0d52c9dd3831b53b746f3
Ilya <iovodovi@yandex.ru>
2025-03-02 20:24:27 +0300

    Обновил тип поля модели отзывов

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type ReviewDocument = HydratedDocument<ReviewModel>;
 
@@ -14,7 +14,7 @@ export class ReviewModel {
 	@Prop()
 	rating: number;
 	@Prop()
-	createdAt: Date;
+	productId: Types.ObjectId;
 }
 
 export const ReviewSchema = SchemaFactory.createForClass(ReviewModel);
