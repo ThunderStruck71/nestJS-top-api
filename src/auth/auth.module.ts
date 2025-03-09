cdfa5c2ad4c2cde006e10cafdb687fce3ac43583
Ilya <iovodovi@yandex.ru>
2025-03-10 00:27:09 +0300

    Добавил кастомный конфиг для jwt токенов

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModel, AuthSchema } from './auth.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from 'src/configs/jwt.config';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: AuthModel.name, schema: AuthSchema }])],
+	imports: [
+		MongooseModule.forFeature([{ name: AuthModel.name, schema: AuthSchema }]),
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig,
+		}),
+	],
 	controllers: [AuthController],
 	providers: [AuthService],
 })
