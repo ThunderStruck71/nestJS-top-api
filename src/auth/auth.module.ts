7a9c3e9349f5b05391fbf2ff38ad82985e9a085c
Ilya <iovodovi@yandex.ru>
2025-03-10 23:45:35 +0300

    Поправил импорты

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { AuthModel, AuthSchema } from './auth.model';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getJwtConfig } from 'src/configs/jwt.config';
+import { getJwtConfig } from '../../src/configs/jwt.config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 
