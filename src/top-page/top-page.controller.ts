9b16b3e67c700450559268e9117e9245b85ed2d2
Ilya <iovodovi@yandex.ru>
2025-03-22 01:40:17 +0300

    Обновил методы апи к топ странице

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,22 +1,58 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	NotFoundException,
+	Param,
+	Patch,
+	Post,
+} from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { TopPageService } from './top-page.service';
+import { TOP_PAGE_NOT_FOUND } from './constants/top-page.constants';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly topPageService: TopPageService) {}
+
 	@Post('create')
-	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
-	
+	async create(@Body() dto: CreateTopPageDto) {
+		return this.topPageService.create(dto);
+	}
+
 	@Get(':id')
-	async get(@Param('id') id: string) {}
-	
+	async get(@Param('id') id: string) {
+		const topPage = await this.topPageService.findById(id);
+		if (!topPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND);
+		}
+		return topPage;
+	}
+
 	@Delete(':id')
-	async delete(@Param('id') id: string) {}
-	
+	async delete(@Param('id') id: string) {
+		const deletedPage = await this.topPageService.deleteById(id);
+		if (!deletedPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND);
+		}
+	}
+
 	@Patch(':id')
-	async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
-	
+	async patch(@Param('id') id: string, @Body() dto: TopPageModel) {
+		const topPage = await this.topPageService.update(id, dto);
+		if (!topPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND);
+		}
+		return topPage;
+	}
+
 	@HttpCode(200)
-	@Post()
-	async find(@Body() dto: FindTopPageDto) {}
+	@Post('find')
+	async find(@Body() dto: FindTopPageDto) {
+		return this.topPageService.findByCategory(dto);
+	}
 }
