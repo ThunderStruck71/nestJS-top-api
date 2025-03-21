832dd46b2d61c04a3805eb1bcb5032d2eaf6402f
Ilya <iovodovi@yandex.ru>
2025-03-22 01:40:00 +0300

    Добавил сервис для обработки данных топ страницы

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
 import { TopPageModel, TopPageSchema } from './top-page.model';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TopPageService } from './top-page.service';
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: TopPageModel.name, schema: TopPageSchema }])],
 	controllers: [TopPageController],
+	providers: [TopPageService],
 })
 export class TopPageModule {}
---
+++ src/top-page/top-page.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { TopPageDocument, TopPageModel } from './top-page.model';
+import { Model } from 'mongoose';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+
+@Injectable()
+export class TopPageService {
+	constructor(
+		@InjectModel(TopPageModel.name) private readonly topPageModel: Model<TopPageDocument>,
+	) {}
+
+	async create(dto: CreateTopPageDto) {
+		return this.topPageModel.create(dto);
+	}
+
+	async findById(id: string) {
+		return this.topPageModel.findById(id).exec();
+	}
+
+	async deleteById(id: string) {
+		return this.topPageModel.findByIdAndDelete(id).exec();
+	}
+
+	async update(id: string, dto: CreateTopPageDto) {
+		return this.topPageModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+	}
+
+	async findByCategory(dto: FindTopPageDto) {
+		return this.topPageModel.find({ firstCategory: dto.firstCategory }).exec();
+	}
+}
