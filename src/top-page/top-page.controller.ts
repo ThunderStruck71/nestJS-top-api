0627853065d651af837f4ec6a0db6eb341111a19
Ilya <iovodovi@yandex.ru>
2025-03-22 16:05:46 +0300

    Добавил авторизацию к методам продуктов и страниц

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -8,6 +8,7 @@ import {
 	Param,
 	Patch,
 	Post,
+	UseGuards,
 	UsePipes,
 	ValidationPipe,
 } from '@nestjs/common';
@@ -17,16 +18,19 @@ import { CreateTopPageDto } from './dto/create-top-page.dto';
 import { TopPageService } from './top-page.service';
 import { TOP_PAGE_NOT_FOUND } from './constants/top-page.constants';
 import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('top-page')
 export class TopPageController {
 	constructor(private readonly topPageService: TopPageService) {}
 
+	@UseGuards(JwtAuthGuard)
 	@Post('create')
 	async create(@Body() dto: CreateTopPageDto) {
 		return this.topPageService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
 	async get(@Param('id', IdValidationPipe) id: string) {
 		const topPage = await this.topPageService.findById(id);
@@ -45,6 +49,7 @@ export class TopPageController {
 		return topPage;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
 	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedPage = await this.topPageService.deleteById(id);
@@ -53,6 +58,7 @@ export class TopPageController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Patch(':id')
 	async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: TopPageModel) {
 		const topPage = await this.topPageService.update(id, dto);
