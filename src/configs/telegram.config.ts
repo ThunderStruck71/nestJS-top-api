710b77fd4b5109fe5493bf537818dcb6dd37a260
Ilya <iovodovi@yandex.ru>
2025-03-30 15:50:20 +0300

    Добавил forRootAsync метод для сервиса telegram

--- src/configs/telegram.config.ts
+++ src/configs/telegram.config.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { ITelegramOptions } from 'src/telegram/telegram.interface';
 
-export const getTelegramConfig = (): ITelegramOptions => {
+export const getTelegramConfig = (configService: ConfigService): ITelegramOptions => {
+	const token = configService.get('TELEGRAM_TOKEN') as string;
+	if (!token) {
+		throw new Error('TELEGRAM_TOKEN не задан');
+	}
+
 	return {
-		chatId: '',
-		token: '',
+		chatId: configService.get('TELEGRAM_CHAT_ID') ?? '',
+		token,
 	};
 };
